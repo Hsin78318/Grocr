73f99b0cae7eabcde79a22cc16bcdd32314ae5d7
hsin78318 <hsin78318@gmail.com>
2018-06-05 18:18:23 +0800

    Firebase installed

---
+++ Grocr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Grocr
+//
+//  Created by Chen Hsin on 2018/6/5.
+//  Copyright © 2018年 Chen Hsin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
